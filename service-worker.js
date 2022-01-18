774d3ba5691f5b5bc282da68236905bcfc8086c3
ccnswap <ccnswap.org@gmail.com>
2022-01-18 18:23:56 +0800

    target

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0e0547bbb3b4045c60947f2044d87482.js"
+  "./precache-manifest.3b28a9c8b160fbda68207786843d3b0d.js"
 );
 
 self.addEventListener('message', (event) => {
